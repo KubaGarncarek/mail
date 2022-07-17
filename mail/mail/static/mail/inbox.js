8174d3e5d6247ec52ff9d17c231ca92bcd3075cb
KubaGarncarek <kubagarncarek47@gmail.com>
2022-07-17 14:19:13 +0200

    add some js

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -54,7 +54,7 @@ function send_email() {
     body: JSON.stringify ({
       recipients: recipients,
       subject: subject,
-      body: body
+      body: body,
     })
   })
   .then(response => response.json())
@@ -68,8 +68,12 @@ function send_email() {
 
 function show_emails(email, mailbox) {
   const email_box = document.createElement('div');
-  email_box.id = "email_box";
   email_box.classList.add("row");
+  if (email.read) {
+    email_box.id = "email_box_unreaded";
+  } else {
+    email_box.id = "email_box_readed";
+  }
 
   const recipient = document.createElement('div');
   recipient.id = "recipient_box";
@@ -84,7 +88,7 @@ function show_emails(email, mailbox) {
 
   const subject = document.createElement('div');
   subject.id = "subject_box";
-  subject.classList.add("col-6");
+  subject.classList.add("col-5");
   subject.innerHTML = email.subject;
   email_box.append(subject);
 
@@ -94,23 +98,99 @@ function show_emails(email, mailbox) {
   timestamp.innerHTML = email.timestamp;
   email_box.append(timestamp);
 
+  if (mailbox !== "sent") {
+    const archive_button = document.createElement('img');
+    archive_button.id = "archive_button";
+    archive_button.src = "static/mail/archive.jpg";
+    archive_button.classList.add("col-1");
+    email_box.append(archive_button);
+    archive_button.addEventListener("click", () => change_email_archive(email.id, email.archived));
+
+  }
+
   document.querySelector('#emails-view').append(email_box);
-  email_box.addEventListener('click', () => view_email(email))
+  recipient.addEventListener('click', () => view_email(email));
+  subject.addEventListener('click', () => view_email(email));
+  timestamp.addEventListener('click', () => view_email(email));
+
+
 }
 
 function view_email(email) {
+    
+  console.clear();
   document.querySelector('#view-email').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#view-email').innerHTML = '';
+  
+  change_to_read(email.id);
 
   fetch(`/emails/${email.id}`)
   .then(response => response.json())
   .then(email => {
     console.log(email);
 
+    const email_view = document.createElement('div');
+    email_view.id = "email_view";
+    const email_info = document.createElement('div');
+    email_info.id = "email_info";
+    email_view.append(email_info);
+
+    const email_sender = document.createElement('h5');
+    email_sender.innerHTML = `From: ${email.sender}`;
+    
+    const email_recipient = document.createElement('h5');
+    email_recipient.innerHTML = `To: ${email.recipients}`;
+   
+    const email_subject = document.createElement('h5');
+    email_subject.innerHTML = `Subject: ${email.subject}`;
+    
+    const email_timestamp = document.createElement('h5');
+    email_timestamp.innerHTML = `Timestamp: ${email.timestamp}`;
+
+    const reply = document.createElement('button');
+    reply.id = 'reply';
+    reply.className = ("btn btn-sm btn-outline-primary");
+    reply.innerHTML = "Reply";
+    
+
+    const hr = document.createElement('hr');
+
+    email_info.append(email_sender, email_recipient, email_subject, email_timestamp, reply, hr);
+
+    const email_body = document.createElement('div');
+    email_body.id = 'email_body';
+    email_body.innerHTML = email.body;
+
+    document.querySelector('#view-email').append(email_view, email_body);
+    
   })
-  const email_view = document.createElement('div');
-  email_view.id = "email_view"
-  email_view.innerHTML = `From: ${email.sender}`;
-  document.querySelector('#view-email').append(email_view);
+  
+  return false;
+  
+
+}
+
+
+function change_email_archive(email_id, old_value) {
+  const new_value = !old_value;
+  fetch(`/emails/${email_id}`, {
+    method : "PUT",
+    body: JSON.stringify ({
+      archived : new_value
+    })
+  })
+  load_mailbox('inbox');
+  window.location.reload();
+}
+
+function change_to_read(email_id) {
+  fetch(`/emails/${email_id}`, {
+    method : "PUT",
+    body: JSON.stringify ({
+      read: true
+    })
+  })
+  return false;
 }
